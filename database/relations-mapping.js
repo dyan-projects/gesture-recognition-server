a2d6edd9d3837c03c9890b10be6a37e9a5e76cdd
dyan tannoo <dyan.tannoo@accenture.com>
2021-08-21 20:58:30 +0400

    minor updates

--- database/relations-mapping.js
+++
@@ -1,9 +0,0 @@
-const mapRelations = db => {
-  // Advert <-> Company
-  db.company?.model.hasMany(db.advert?.model);
-  db.advert?.model.belongsTo(db.company?.model);
-
-  return db;
-};
-
-module.exports = mapRelations;
