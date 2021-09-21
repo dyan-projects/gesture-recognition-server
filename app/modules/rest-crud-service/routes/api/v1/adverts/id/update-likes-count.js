9005266a44e89805f5e32f77320756eabd367a2d
dyan tannoo <dyan.tannoo@accenture.com>
2021-09-21 17:13:24 +0400

    minor fix

--- app/modules/rest-crud-service/routes/api/v1/adverts/id/update-likes-count.js
+++ app/modules/rest-crud-service/routes/api/v1/adverts/id/update-likes-count.js
@@ -5,6 +5,8 @@ module.exports = Router({ mergeParams: true }).put('/v1/adverts/:id/likes', (req
     req.db.advert.findByPk(req.params.id).then(advert => {
       advert.likes += req.body.like;
       advert.dislikes += req.body.dislike;
+      if (advert.likes < 0) advert.likes = 0;
+      if (advert.dislikes < 0) advert.dislikes = 0;
       advert.save().then(() => res.status(200).send(advert));
     });
   } catch (err) {
